5a8996dab264ed4c09f70f6c74efdd404efa4a23
lim <qqqnqlt123@naver.com>
2022-09-13 18:24:55 +0900

    tag List로 바꾸고 merging_resolve 오류난거 수정(tagdao 둘다수정해서 오류남)

--- screen/src/main/java/dao/TagDao.java
+++ screen/src/main/java/dao/TagDao.java
@@ -1,5 +1,6 @@
 package dao;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,75 +10,80 @@ import java.util.List;
 
 import common.OracleConn;
 import dto.Tag;
+import oracle.jdbc.oracore.OracleType;
 
 public class TagDao {
-	private final Connection conn =OracleConn.getInstance().getConn();
-	PreparedStatement stmt;
+private final Connection conn =OracleConn.getInstance().getConn();
+PreparedStatement stmt;
+CallableStatement cstmt;
+public List<Tag> searchBox(){
+	List<Tag> searchTag = new ArrayList<Tag>();
+	String sql ="select top_seq,top,mid_seq,mid,seqno,name from v_tag_info";
+
+	try {
+		stmt = conn.prepareStatement(sql,ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
+		ResultSet rs =stmt.executeQuery();
+
 	
-	public List<Tag> searchBox(){
-		List<Tag> searchTag = new ArrayList<Tag>();
-		String sql ="select top_seq,top,mid_seq,mid,seqno,name from v_tag_info";
-		
-		try {
-			stmt = conn.prepareStatement(sql,ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
-			ResultSet rs =stmt.executeQuery();
-			
-			
-			
-			while(rs.next()) {
-				Tag sd = new Tag();
-				sd.setTop(rs.getString("top"));
-				sd.setMid(rs.getString("mid"));
-				sd.setName(rs.getString("name"));
-				sd.setMidSeq(rs.getString("mid_seq"));
-				sd.setTopSeq(rs.getString("top_seq"));
-				sd.setSeqno(rs.getString("seqno"));
-				searchTag.add(sd);
-			}
-			rs.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
+		while(rs.next()) {
+			Tag sd = new Tag();
+			sd.setTop(rs.getString("top"));
+			sd.setMid(rs.getString("mid"));
+			sd.setName(rs.getString("name"));
+			sd.setMidSeq(rs.getString("mid_seq"));
+			sd.setTopSeq(rs.getString("top_seq"));
+			sd.setSeqno(rs.getString("seqno"));
+			searchTag.add(sd);
 		}
-		return searchTag;
-		
+		rs.close();
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+	return searchTag;
+
+}
+
+public int insert(String tagName) {
+	int rs=0;
+	String sql ="call p_insert_tag(?,?)";
+	try {
+		cstmt = conn.prepareCall(sql);
+		cstmt.setString(1, tagName);
+		cstmt.registerOutParameter(2, OracleType.STYLE_INT);
+		cstmt.executeQuery();
+		rs=cstmt.getInt(2);
+	} catch (SQLException e) {
+		e.printStackTrace();
 	}
 
-	public void insert(String tagName) {
-		String sql ="INSERT INTO tag VALUES (tag_seqno.NEXTVAL,?)";
-		try {
-			stmt = conn.prepareStatement(sql);
-			stmt.setString(1, tagName);
-			stmt.executeQuery();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+	return rs;
+}
 
-	public void modify(String seqno, String newName) {
-		//쓰읍... 제대로 하려면 중복검사도 해야할거 같긴한데...
-		String sql ="UPDATE tag SET name=? WHERE seqno=?";
-		try {
-			stmt = conn.prepareStatement(sql);
-			stmt.setString(1, newName);
-			stmt.setString(2, seqno);
-			stmt.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+public int modify(String seqno, String newName) {
+	int rs=0;
+	String sql ="call p_modify_tag(?,?,?)";
+	try {
+		cstmt = conn.prepareCall(sql);
+		cstmt.setString(1, seqno);
+		cstmt.setString(2, newName);
+		cstmt.registerOutParameter(3, OracleType.STYLE_INT);
+		cstmt.executeUpdate();
+		rs=cstmt.getInt(3);
+	} catch (SQLException e) {
+		e.printStackTrace();
 	}
+	return rs;
+}
 
-	public void delete(String seqno) {
-		String sql ="DELETE FROM tag_match WHERE tag_seqno=?";
-		try {
-			stmt = conn.prepareStatement(sql);
-			stmt.setString(1, seqno);
-			stmt.executeUpdate();
-			sql="DELETE FROM tag WHERE seqno=?";
-			stmt = conn.prepareStatement(sql);
-			stmt.setString(1, seqno);
-			stmt.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+public void delete(String seqno) {
+	String sql ="call p_delete_tag(?)";
+	try {
+		cstmt = conn.prepareCall(sql);
+		cstmt.setString(1, seqno);
+		cstmt.executeUpdate();
+
+	} catch (SQLException e) {
+		e.printStackTrace();
 	}
+}
 }
